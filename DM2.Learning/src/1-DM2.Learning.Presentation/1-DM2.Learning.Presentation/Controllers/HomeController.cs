384158ce97c6a0430c1535a982d8a9a3ad482e5a
masanori-iha <masanori.iha@outlook.com>
2023-05-29 20:19:41 -0300

    [FEAT] - Criando estrutura inical DDD

---
+++ DM2.Learning/src/1-DM2.Learning.Presentation/1-DM2.Learning.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using _1_DM2.Learning.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _1_DM2.Learning.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DM2.Learning/src/1-DM2.Learning.Presentation/1-DM2.Learning.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1_DM2.Learning.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
