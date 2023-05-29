e69e7d49c43670ef596811894d860fb1d14a1faf
Daniel Antonio de Campos <daniel_a_campos@outlook.com>
2023-05-29 20:33:23 -0300

    teste commit

--- DM2.Learning/src/1-DM2.Learning.Presentation/1-DM2.Learning.Presentation/Controllers/HomeController.cs
+++ DM2.Learning/src/1-DM2.Learning.Presentation/1-DM2.Learning.Presentation/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace _1_DM2.Learning.Presentation.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
