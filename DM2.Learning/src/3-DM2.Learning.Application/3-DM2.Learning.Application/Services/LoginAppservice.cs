dde3e47d11c62901baa03808f56ce0e8de8bf54c
Daniel Antonio de Campos <daniel_a_campos@outlook.com>
2023-05-29 21:09:00 -0300

    [FEAT] - Creating LoginAppService

---
+++ DM2.Learning/src/3-DM2.Learning.Application/3-DM2.Learning.Application/Services/LoginAppservice.cs
@@ -0,0 +1,18 @@
+﻿using _3_DM2.Learning.Application.interfaces;
+using _3_DM2.Learning.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_DM2.Learning.Application.Services
+{
+    public class LoginAppservice : ILoginAppService
+    {
+        public Task<AuthenticateTokenViewModel> Authenticate(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
