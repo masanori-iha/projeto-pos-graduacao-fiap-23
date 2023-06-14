2cc870dee297386b51a572a12e711a306c08f4a6
Daniel Antonio de Campos <Daniel_A_campos@outlook.com>
2023-06-13 21:05:43 -0300

    [FEAT] - Starting create UserRepository

--- DM2.Learning/src/5-DM2.Learning.Infra/5-DM2.Learning.Infra/Repositories/UserRepository.cs
+++ DM2.Learning/src/5-DM2.Learning.Infra/5-DM2.Learning.Infra/Repositories/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using _4_DM2.Learning.Domain.IdentityModels;
 using _4_DM2.Learning.Domain.Interfaces.Repositories;
+using _5_DM2.Learning.Infra.Context;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,20 @@ namespace _5_DM2.Learning.Infra.Repositories
 {
     public class UserRepository : IUserRpository
     {
+        private readonly DM2Context _context;
+        public UserRepository(DM2Context dM2Context)
+        {
+            _context = dM2Context;
+        }
         public async Task<bool> ValidateUser(string login, string password)
         {
             return true;
         }
+
+        public async Task<User> GetUserById(Guid id)
+        {
+            return await _context.FindAsync<User>(id);
+        }
+        
     }
 }
